f8e4d44168aa63abc1ff2ff8fa7b0d00da6da127
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2022-10-04 12:59:44 +0300

    deleted contacts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,7 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Ronald Reagan', number: '459-12-56' },
-      { id: 'id-2', name: 'Boris Johnson', number: '443-89-12' },
-      { id: 'id-3', name: 'Volodymyr Zelensky', number: '645-17-79' },
-      { id: 'id-4', name: 'Winston Churchill', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
