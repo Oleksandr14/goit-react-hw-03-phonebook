bcc3ed1bf1118a2390f257d5eb3a7ebb01b09c23
Oleksandr Palamarchuk <palamarchuk.oa93@gmail.com>
2022-10-04 12:44:55 +0300

    add contacts to storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,26 @@ import { Filter } from './Filter/Filter';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contact = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contact);
+
+    if (parseContacts) {
+      this.setState({ contacts: parseContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+
+    if (contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
 
   addContact = newContact => {
     this.setState(prev => {
